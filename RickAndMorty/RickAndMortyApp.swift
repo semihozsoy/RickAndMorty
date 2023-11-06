479f0c1063ec6d5d2cd3fa0491bcc44379e5daa9
Semih Özsoy <semih__ozsoy@hotmail.com>
2023-11-06 15:54:47 +0300

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Semih Ozsoy on 6.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
