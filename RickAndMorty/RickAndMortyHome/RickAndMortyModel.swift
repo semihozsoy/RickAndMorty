95cb8ca5db55848c63ae00c476ca671ec95a9e61
Semih Özsoy <semih__ozsoy@hotmail.com>
2023-11-09 18:17:15 +0300

    RickAndMorty scene implemented, model added

---
+++ RickAndMorty/RickAndMortyHome/RickAndMortyModel.swift
@@ -0,0 +1,59 @@
+//
+//  RickAndMortyModel.swift
+//  RickAndMorty
+//
+//  Created by Semih Ozsoy on 7.11.2023.
+//
+
+import Foundation
+
+// MARK: - RickAndMorty
+struct RickAndMorty: Codable {
+    let info: Info?
+    let results: [RickAndMortyResult]?
+}
+
+// MARK: - Info
+struct Info: Codable {
+    let count, pages: Int?
+    let next: String?
+    let prev: String?
+}
+
+// MARK: - RickAndMortyResult
+struct RickAndMortyResult: Codable {
+    let id: Int?
+    let name: String?
+    let status: Status?
+    let species: Species?
+    let type: String?
+    let gender: Gender?
+    let origin, location: Location?
+    let image: String?
+    let episode: [String]?
+    let url: String?
+    let created: String?
+}
+
+enum Gender: String, Codable {
+    case female = "Female"
+    case male = "Male"
+    case unknown = "unknown"
+}
+
+// MARK: - Location
+struct Location: Codable {
+    let name: String?
+    let url: String?
+}
+
+enum Species: String, Codable {
+    case alien = "Alien"
+    case human = "Human"
+}
+
+enum Status: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case unknown = "unknown"
+}
---
+++ RickAndMorty/RickAndMortyHome/RickAndMortyView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  RickAndMorty
+//
+//  Created by Semih Ozsoy on 6.11.2023.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct RickAndMortyView: View {
+    @StateObject var viewModel = RickAndMortyViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List(viewModel.results, id: \.id) { item in
+                // NavigationLink wrapping our cell.
+                NavigationLink {
+                    RickAndMortyDetailView(result: item)
+                } label: {
+                    CharacterCell(result: item)
+                }
+            }.task {
+                await viewModel.getCharacters()
+             }
+            .navigationTitle("Rick and Morty")
+        }
+    }
+}
+
+struct RickAndMortyView_Previews: PreviewProvider {
+    static var previews: some View {
+        RickAndMortyView()
+    }
+}
